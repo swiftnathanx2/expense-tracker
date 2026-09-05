97875eba5044b0ba1dd47cfd65aca00871387272
Valery <Nyoh Jonathan Smith>
2026-09-05 09:41:40 +0100

    feat: add transaction context component

---
+++ src/context/TransactionContext.jsx
@@ -0,0 +1,60 @@
+import { createContext, useState } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+import defaultCategories from "../data/categories";
+
+export const TransactionsContext = createContext(null);
+
+export function TransactionProvider({ children }) {
+  const [transactions, setTransactions] = useLocalStorage(transactions, []);
+
+  const [customCategories, setCustomCategories] = useLocalStorage(
+    customCategories,
+    [],
+  );
+
+  const [editId, setEditId] = useState(null);
+
+  const [selectedMonth, setSelectedMonth] = useState(
+    new Date().toISOString().slice(0, 7),
+  );
+
+  const addOrUpdateTransaction = (formData) => {
+    if (editId) {
+      setTransactions((prev) =>
+        prev.map((t) => (t.id === editId ? { ...t, ...formData } : t)),
+      );
+      setEditId(null);
+    } else {
+      setTransactions((prev) => [
+        { id: crypto.randomUUID(), ...formData },
+        ...prev,
+      ]);
+    }
+  };
+
+  const deleteTransaction = (id) => {
+    setTransactions((prev) => prev.filter((t) => t.id !== id));
+  };
+
+  const categories = [...defaultCategoriesData, ...customCategories];
+
+  const value = {
+    transactions,
+    categories,
+    setCustomCategories,
+    editId,
+    setEditId,
+    selectedMonth,
+    setSelectedMonth,
+    addOrUpdateTransaction,
+    deleteTransaction,
+  };
+
+  return;
+
+  <div>
+    <TransactionsContext.Provider value={value}>
+      {children}
+    </TransactionsContext.Provider>
+  </div>;
+}
